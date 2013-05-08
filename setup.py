a5db75f5b72035e3719964e0581f39e81d239980
Andrew Kelleher <akellehe@gmail.com>
2013-05-08 07:36:23 -0400

    Added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,19 @@ except ImportError:
 
 setup(
   name='graphism',
-  version='1.0.1',
+  version='1.0.3',
   packages=find_packages(),
   author='Andrew Kelleher',
   author_email='akellehe@gmail.com',
   description='Basic network graph test package',
-  test_suite='graphism.tests'
+  test_suite='graphism.tests',
+  install_requires=[
+    "Jinja2==2.6",
+    "Pygments==1.6",
+    "Sphinx==1.2b1",
+    "docutils==0.10",
+    "pyglet==1.1.4",
+    "wsgiref==0.1.2"
+  ]
+
 )
