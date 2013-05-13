c387c00c4ae75b686aa169d66c756b1c001abefc
Adam Kelleher <akelleh@gmail.com>
2013-05-13 14:47:51 -0400

    commented all the code

--- graphism/examples/SI_model.py
+++ graphism/examples/SI_model.py
@@ -2,13 +2,24 @@ import graphism.graph as gg
 import graphism.graph_generators.uniform_graph as ug
 
 def cb_fun( node ):
+  """
+  super simple callback function
+  """
   return 0
 
+# set the parameters for the uniform graph
 N = 300
+
+# build the graph
 g = ug.build_uniform_graph( N )
+
+#set the callback function for the cascade
 g.set_infection( cb_fun )
 
+# choose some nodes to start out infected
 g.infect_seeds( set([g.get_node_by_name('1'), g.get_node_by_name('2'),g.get_node_by_name('3'),g.get_node_by_name('4')]) )
+
+# while there are still infected nodes, propagate the cascade.  print the volume infected at each step to stdout
 while( len(g.infected() ) > 0 ):
   g.propagate()
   print len( g.infected() )
