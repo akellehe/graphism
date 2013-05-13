9fbbaaaa8160a25ffdd0e006fee4a968a37de6bf
Adam Kelleher <akelleh@gmail.com>
2013-05-13 14:26:29 -0400

    changed graphs directory to graph_generators

--- graphism/examples/SI_model.py
+++ graphism/examples/SI_model.py
@@ -1,5 +1,5 @@
 import graphism.graph as gg
-import uniform_graph as ug
+import graphism.graph_generators.uniform_graph as ug
 
 def cb_fun( node ):
   return 0
@@ -8,7 +8,7 @@ N = 300
 g = ug.build_uniform_graph( N )
 g.set_infection( cb_fun )
 
-g.infect_seeds( set([g.get_node_by_name('1')]) )
-while( len(g.infected() ) < N ):
+g.infect_seeds( set([g.get_node_by_name('1'), g.get_node_by_name('2'),g.get_node_by_name('3'),g.get_node_by_name('4')]) )
+while( len(g.infected() ) > 0 ):
   g.propagate()
   print len( g.infected() )
