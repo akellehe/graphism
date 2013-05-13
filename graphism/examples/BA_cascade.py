76cdd26abb4bec09ff1d7a99cb913ae8bedb8286
Adam Kelleher <akelleh@gmail.com>
2013-05-13 12:30:53 -0400

    BA generator now takes seed graphs using optional argument seed_graph, finished BA_cascade example

--- graphism/examples/BA_cascade.py
+++ graphism/examples/BA_cascade.py
@@ -1,11 +1,15 @@
 import graphism.graph as gg
+import graphism.graphs.barabasi_albert as ba
 import graphism.graphs.uniform_graph as ug
 
 def cb_fun( node ):
   return 0
 
-N = 50
-g = ug.build_uniform_graph( N )
+m = 2   # edges to add at each step
+N = 50  # total number of nodes
+
+seed_graph = ug.build_uniform_graph( 10 )
+g = ba.barabasi_albert( m, N, seed_graph )
 g.set_infection( cb_fun )
 g.set_recovery( cb_fun )
 
