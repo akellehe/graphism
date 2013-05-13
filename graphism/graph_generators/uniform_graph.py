37511e998202cc81a267f3c1f502c1211fbe3dcf
Adam Kelleher <akelleh@gmail.com>
2013-05-13 14:48:31 -0400

    commented all the code

---
+++ graphism/graph_generators/uniform_graph.py
@@ -0,0 +1,33 @@
+import graphism.graph as gg
+
+
+def trans_prob(a, b):
+  """
+  a simple transmission function, returning probability p = 0.5 for node a to infcet node b
+
+  Required parameters:
+  :param graphism.Node a: the infected node
+  :param graphism.Node b: the susceptible node
+  """
+
+  return 0.5
+
+def build_uniform_graph( N ):
+  """
+  Takes N, the number of nodes to be in the final graph, and returns a graph where each node is connected by one edge to every other node.  There are no self edges.  This is the graph assumed by the SIR model, so this graph can be used to simulate that model.
+
+  Required parameters:
+  :param int N: the number of nodes in the final graph
+  """
+  N = N + 1
+  edgelist = []
+  nodelist = range( N )
+  nodelist.remove( 0 )
+  for i in nodelist:
+    for j in nodelist:
+      if i != j:
+        edgelist.append((str(i),str(j) ))
+  
+  g = gg.Graph( edgelist, transmission_probability=trans_prob )
+  return g
+
