710cfa350e1302bfcd83abc557bf4785946af8d6
Adam Kelleher <akelleh@gmail.com>
2013-05-28 12:51:55 -0400

    added tp and rp change to BA cascade

--- graphism/examples/BA_cascade.py
+++ graphism/examples/BA_cascade.py
@@ -16,17 +16,25 @@ def cb_fun( node ):
   """
   return 0
 
+def tp_fun( node1, node2 ):
+  return 0.9
+
+def rp_fun( node ):
+  return 0.1
+
 # set the parameters for the BA graph generator
 m = 2   # edges to add at each step
-N = 50  # total number of nodes
+N = 200  # total number of nodes
 
 # just for fun, build the BA graph from a uniform seed graph
 seed_graph = ug.build( 10 )
 g = ba.barabasi_albert( m, N, seed_graph )
 
-# set the callback functions for the cascades
+# set the callback and epidemic functions for the cascades
 g.set_infection( cb_fun )
 g.set_recovery( cb_fun )
+g.set_transmission_probability( tp_fun )
+g.set_recovery_probability( rp_fun )
 
 # start the cascade with these nodes infected
 g.infect_seeds( set([g.get_node_by_name('1'), g.get_node_by_name('2'), g.get_node_by_name('3') ]) )
