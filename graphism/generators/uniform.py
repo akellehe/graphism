5b4243414c2ea302c025262fb08ac789dc989732
Andrew Kelleher <akellehe@gmail.com>
2013-05-30 15:52:17 -0400

    Cleanups to uniform graph generator

--- graphism/generators/uniform.py
+++ graphism/generators/uniform.py
@@ -1,31 +1,30 @@
 import graphism.graph as gg
 
 def trans_prob(a, b):
-  """
-  a simple transmission function, returning probability p = 0.5 for node a to infcet node b
-
-  :param graphism.node.Node a: the infected node
-  :param graphism.node.Node b: the susceptible node
-  """
-
-  return 0.9
+    """
+    A simple transmission function, returning probability p = 0.9 for node a to infect node b
+    
+    :param graphism.node.Node a: the infected node
+    :param graphism.node.Node b: the susceptible node
+    
+    :rtype float: A constant transmission probability of 90 percent
+    """
+    return 0.9
 
 def build( n ):
-  """
-  Takes n, the number of nodes to be in the final graph, and returns a graph where each node is connected by one edge to every other node.  There are no self edges.  This is the graph assumed by the SIR model, so this graph can be used to simulate that model.
-
-  :param int n: the number of nodes in the final graph
-  :rtype graphism.graph.Graph: a uniform graph
-  """
-  n = n + 1
-  edgelist = []
-  nodelist = range( n )
-  nodelist.remove( 0 )
-  for i in nodelist:
-    for j in nodelist:
-      if i != j:
-        edgelist.append((str(i),str(j) ))
-  
-  g = gg.Graph( edgelist, transmission_probability=trans_prob )
-  return g
-
+    """
+    Takes n, the number of nodes to be in the final graph, and returns a graph where each node is connected by one edge to every other node.  There are no self edges.  This is the graph assumed by the SIR model, so this graph can be used to simulate that model.
+    
+    :param int n: the number of nodes in the final graph
+    
+    :rtype graphism.graph.Graph: a uniform graph
+    """
+    edgelist = []
+    nodelist = range(1,n+1)
+    for i in nodelist:
+        for j in nodelist:
+            if i != j:
+                edgelist.append((str(i),str(j)))
+                
+    return gg.Graph( edgelist, transmission_probability=trans_prob )
+    
