05ff1d42b15a374224923a5c8f6cba2dc6abdc00
Andrew Kelleher <akellehe@gmail.com>
2013-06-11 12:41:37 -0400

    Started rewriting core storage mechanisms for graph object

--- graphism/graph.py
+++ graphism/graph.py
@@ -30,381 +30,78 @@ class Graph(object):
     :param list(dict) edges: You can optionally pass the graph as a keyword argument instead of the first positional argument.
     
     """
+    __nodes = None
+    __edges = None
+    
     __susceptible = None
     __infected = None
     __recovered = None
     
-    __transmission_probability = None
-    __recovery_probability = None
-    
-    __infection = None
-    __recovery = None
-    
-    __length = None
-    
-    def __init__(self, *args, **kwargs):
-        self.__susceptible = {}
-        self.__infected = {}
-        self.__recovered = {}
+    def _simple_init(self, edges):
+        for parent, child in edges:
+            self.add_edge(parent, child)
                 
-        self.__length = kwargs.get('length', None)
-                
-        self.__transmission_probability = kwargs.get('transmission_probability', tp)
-        self.__recovery_probability = kwargs.get('recovery_probability', rp)
-                
-        if 'edges' in kwargs:
-            self.__init_nodes_from_kwargs(kwargs)
-        elif args:
-            self.__init_nodes_from_args(args, kwargs)
-            
-        self.set_infection(kwargs.get('infection', return_none_from_one))
-        self.set_recovery(kwargs.get('recovery', return_none_from_one))
+    def _complex_init(self, edges):
+        for edge_metadata in edges:
+            self.add_edge(**edge_metadata)
     
-    def add_edge_by_node_sequence(self, parent, child, directed=None, transmission_probability=None, type_=None, weight_=1.0, recovery_probability=None):
-        """
-        Creates nodes and an edge between two nodes for a given name.
-        
-        :param str parent: The name for the parent node
-        :param str child: The name for the child node
-        :param bool directed: Whether or not the edge should be directed.
-        :param function transmission_probability: The transmission probability function. Should take two arguments of type graphism.node.Node. The first positional argument is the parent (infection host), the second is the child. 
-        :param function recovery_probability: The recovery probability function. Should take a single objet of type graphism.node.Node. Returns a float on [0,1] indicating the probability of recovery for the node.
-        :param str type_: The type of edge
-        :param str weight_: The weight of the edge
-        """
-        p = self.get_node_by_name(parent)
-        if not p:
-            p = Node(name=parent,
-                     transmission_probability=transmission_probability or self.__transmission_probability,
-                     recovery_probability=recovery_probability or self.__recovery_probability,
-                     graph=self,
-                     length=self.__length)
+    def __init__(self, edges, directed=False, transmission_probability=None, recovery_probability=None, infection=None, recovery=None):
+        self.__nodes = {}
+        self.__edges = {}
 
-        c = self.get_node_by_name(child)
-        if not c:
-            c = Node(name=child,
-                     transmission_probability=transmission_probability or self.__transmission_probability,
-                     recovery_probability=recovery_probability or self.__recovery_probability,
-                     graph=self,
-                     length=self.__length)
+        self.__susceptible = set([])
+        self.__infected = set([])
+        self.__recovered = set([])
         
-        p.add_child(c, type_=type_, weight_=weight_)
-        
-        self.add_node(p)
-        self.add_node(c)
-    
-    def __init_nodes_from_kwargs(self, kwargs):
-        """
-        Initializes internal nodes for a set of keyword arguments.
-        
-        :param dict kwargs: The keyword arguments for the __init__ method.
-        
-        """
-        edges = kwargs['edges']
-        directed = kwargs.get('directed', False)
-        
-        for edge in edges:
-            parent = edge['from_']
-            child = edge['to_']
-            type_ = edge.get('type_', None)
-            weight_ = edge.get('weight_', 1.0)
-            
-            self.add_edge_by_node_sequence(parent=parent, 
-                                           child=child, 
-                                           directed=directed, 
-                                           transmission_probability=self.__transmission_probability, 
-                                           type_=type_, 
-                                           weight_=weight_, 
-                                           recovery_probability=self.__recovery_probability)
-        
-    def __init_nodes_from_args(self, args, kwargs):
-        """
-        Initializes internal nodes for a set of positional and keyword arguments.
-        
-        :param tuple args: The positional arguments for the __init__ method.
-        :param dict kwargs: The keyword arguments for the __init__ method.
-        
-        """
-        graph = args[0]
-        directed = kwargs.get('directed', False)
-        for edge in graph:
-            parent, child = edge
-            self.add_edge_by_node_sequence(parent=parent, 
-                                           child=child,
-                                           directed=directed,
-                                           transmission_probability=self.__transmission_probability,
-                                           recovery_probability=self.__recovery_probability)
-    
-    def set_transmission_probability(self, f):
-        """
-        Allows the user to set the transmission probability function for all nodes in the graph to f
-        
-        :param function f: The new transmission probability function to use for all nodes in the graph.
-        """
-        self.__transmission_probability = f
-        for n in self.nodes():
-            n.set_transmission_probability(f)
-            
-    def set_recovery_probability(self, f):
-        """
-        Allows the user to set the recovery probability function for all nodes in the graph to f
-        
-        :param function f: The new recovery probability function to use for all nodes in the graph.
-        """
-        self.__recovery_probability = f
-        for n in self.nodes():
-            n.set_recovery_probability(f)
-            
-    def get_transmission_probability(self):
-        """
-        Getter for the graph's transmission probability function.
-        
-        :rtype function:
-        """
-        return self.__transmission_probability
-    
-    def get_recovery_probability(self):
-        """
-        Getter for the graph's recovery probability function
-        
-        :rtype function:
-        """
-        return self.__recovery_probability
-    
-    def get_node_by_name(self, name):
-        """
-        Searches the internal dict maintaining the ONLY strong reference to internal nodes by name. Returns the node with that name.
-        
-        :param str name: The name of the node to return.
-        
-        :rtype graphism.node.Node:
-        """
-        return self.__susceptible.get(name, self.__infected.get(name, self.__recovered.get(name, None)))
-    
-    def add_node(self, node):
-        """
-        Adds a node to the graph.
-        
-        :param graphism.node.Node node: The node to add.
-        
-        :rtype graphism.node.Node: 
-        """
-        if node.name() not in self.__susceptible:
-            self.__susceptible[node.name()] = node
-        return node
-        
-    def add_edge(self, from_, to_):
-        """
-        Creates an edge between two nodes in the graph.
-        
-        :param graphism.node.Node from_: The node to add an edge from. (parent)
-        :param graphism.node.Node to_: The terminal node. (child)
-        
-        :rtype tuple(graphism.node.Node, graphism.node.Edge, graphism.node.Node: A tuple of the parent node, edge, and child node.
-        """
-        if from_ not in self.nodes():
-            self.add_node(from_)
-        if to_ not in self.nodes():
-            self.add_node(to_)
-            
-        from_.add_child(to_)
-        
-        return (from_, from_.edges()[to_.name()], to_)
-        
-        
-    def set_infection(self, callback):
-        """
-        Sets the infection function to callback. Should return True if the node was infected. False otherwise.
+        if len(edges[0]) == 2: # For simple edge lists:
+            self._simple_init(edges, directed, transmission_probability, recovery_probability, infection, recovery)
+        else: # For full edge exports:
+            self._complex_init(edges, directed, transmission_probability, recovery_probability, infection, recovery)
 
-        :param function callback: The function to execute on a node being infected. The only argument should be the node itself.
-        
-        :rtype None:
+    def edge(self, parent, child):
         """
-        self.__infection = callback
+        Returns an edge by the parent and child names
         
-    def set_recovery(self, callback):
+        :param str parent: The name of the parent node
+        :param str child: The name of the child node
+        
+        :rtype graphism.edge.Edge: The edge or None if it does not exist
         """
-        Sets the recovery function to callback. Should return True if the node recovers. False otherwise
+        if parent in self.__edges and child in self.__edges[parent]:
+            return self.__edges[parent][child]
+        else:
+            return None
 
-        :param function callback: The function to execute on a node being infected. The only argument should be the node itself.
+    def add_edge(self, parent, child, weight=1.0, multiplicity=1L):
+        """
+        Adds an edge to the graph
         
-        :rtype None:
-        """           
-        self.__recovery = callback
+        :param str parent: The name of the parent node.
+        :param str child: The name of the child node
+        :param float weight: The weight on the edge.
+        :param long multiplicity: The multiplicity of the edge.
         
-    def infect_seeds(self, seed_nodes):
+        :rtype graphism.edge.Edge: The edge added to the graph.
         """
-        Infects the seed_nodes by executing the infect method for those nodes.
+        edge = self.edge(parent, child)
+        if edge:
+            edge.multiplicity += 1L
+        else:
+            edge = Edge(parent, child, multiplicity=multiplicity, weight=weight, graph=self)
+            if parent not in self.__nodes:
+                self.__nodes[parent] = Node(parent, graph=self)
+            if child not in self.__nodes:
+                self.__nodes[child] = Node(child, graph=self)
+            if parent in self.__edges:
+                self.__edges[parent][child] = edge
+            else:
+                self.__edges[parent] = {child: edge}
+        return edge
         
-        :param set(graphism.node.Node) seed_nodes: The nodes to start the infection with.
-        """
-        for n in seed_nodes:
-            n.infect(self.__infection)
-            
-    def infected(self):
-        """
-        Returns the set of infected nodes in the graph.
-        
-        :rtype set(graphism.node.Node):
-        """
-        return set(self.__infected.values())
-    
-    def add_infected(self, node):
-        """
-        Adds a node to the list of infected nodes
-        
-        """
-        self.__infected[node.name()] = node
-        
-    def add_recovered(self, node):
-        """
-        Adds a node to the list of recovered nodes
-        
-        """
-        self.__recovered[node.name()] = node
-        
-    def remove_infected(self, node):
-        """
-        Removes a node from the list of infected nodes
-        
-        """
-        del self.__infected[node.name()]
-            
-    def remove_susceptible(self, node):
-        """
-        Removes a node from the list of susceptible nodes.
-        
-        """
-        del self.__susceptible[node.name()]
-    
     def nodes(self):
         """
-        Returns the internal nodes as a set. Deprecated.
+        Returns a unique list of all the nodes in the graph.
         
-        :rtype set(graphism.node.Node):
+        :rtype list(graphism.node.Node):
         """
-        return self.susceptible().union(self.infected()).union(set(self.__recovered.values()))
-    
-    def is_susceptible(self, node):
-        """
-        Indicates if the node is or isn't susceptible to infection
-        
-        :param graphism.node.Node node: The node to indicate the status of
-        
-        :rtype bool: True if the node is susceptible. False otherwise
-        """
-        return node.name() in self.__susceptible
-
-    def susceptible(self):
-        """
-        Returns the set of susceptible nodes in the graph.
-
-        :rtype set(graphism.node.Node):
-        """
-        return set(self.__susceptible.values())
-
-    def recovered(self):
-        """
-        Returns the set of recovered nodes in the graph.
-
-        :rtype set(graphism.node.Node):
-        """
-        return set(self.__recovered.values())
-
-    def propagate(self):
-        """
-        First infects nodes according to the probability of transmission. 
-        Second, recovers nodes depending on the probability of recovery. 
-
-        """
-        for n in self.infected():
-            n.propagate_infection(self.__infection)
-
-        self.recover()
-
-    def closeness(self, a, b):
-        """
-        Finds the shortest distance between a and b.
-
-        :param graphism.node.Node a: The first node.
-        :param graphism.node.Node b: The second node.
-
-        :rtype tuple(float, list(str)): The closeness and a list of predecessor ids
-        """
-        final_distances = {}
-        predecessors = []
-        non_final_distances = priorityDictionary()
-        non_final_distances[a.name()] = 0.0
-
-        for node_name in non_final_distances:
-            final_distances[node_name] = non_final_distances[node_name]
-            if node_name == b.name():
-                break
-
-            for node in self[node_name]:
-                length = final_distances[node_name] + self[node_name][node.name()].length()
-                if node.name() in final_distances:
-                    if length < final_distances[node.name()]:
-                        raise ValueError("Found better path to already-final vertex")
-                elif node.name() not in non_final_distances or length < non_final_distances[node.name()]:
-                    non_final_distances[node.name()] = length
-                    predecessors.append(node_name)
-
-        return (final_distances[b.name()], predecessors)
-
-    def __iter__(self):
-        """
-        Returns a generator over nodes in the graph.
-        
-        """
-        for node in self.nodes():
-            yield node
-
-    def __getattr__(self, node_name):
-        """
-        Returns the node corresponding to node_name
-        
-        :rtype graphism.node.Node: 
-        """
-        return self.get_node_by_name(node_name)
-
-    def __getitem__(self, node_name):
-        """
-        Returns the node corresponding to node_name
-        
-        :rtype graphism.node.Node: 
-        """
-        return self.__getattr__(node_name)
-    
-    def edges(self):
-        """
-        Returns the set of all the edges in the graph.
-        
-        :rtype set(graphism.edge.Edge):
-        """
-        edges = set([])
-        for n in self.nodes():
-            [edges.add(e) for e in set(n.edges().values())]
-            
-        return edges
-    
-    def export(self):
-        """
-        Returns the edgelist as a list of dictionaries to be used for initializing a new graph.
-        
-        :rtype list(dict): Edges with attributes to be re-created.
-        """
-        return [e.to_dict() for e in self.edges()]
-
-    def recover(self):
-        """
-        Executes the recovery function for each infected node and subsequently 
-        removes it from the 'infected' set iff that node recovered.
-
-        """
-        for n in self.infected():
-            if n.recover(self.__recovery): # Returns true if recovered, false if not
-                self.remove_infected(n)
-                self.add_recovered(n)
-
-
+        return self.__nodes.values()
